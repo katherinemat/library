ae5d8fc763404d8cc37154ab2ad405f7e37c001c
Derek Villars and Kaz Matthews <derek+kaz@>
2017-03-01 10:43:28 -0800

    add delete, update, find, and search methods with passing tests

--- Objects/Author.cs
+++ Objects/Author.cs
@@ -85,6 +85,93 @@ namespace Library
             DB.CloseSqlConnection(rdr, conn);
         }
 
+        public static Author Find(int id)
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM author WHERE id = @AuthorId;", conn);
+            cmd.Parameters.Add(new SqlParameter("@AuthorId", id.ToString()));
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            int foundAuthorId = 0;
+            string foundName = null;
+
+            while(rdr.Read())
+            {
+                foundAuthorId = rdr.GetInt32(0);
+                foundName = rdr.GetString(1);
+            }
+            Author foundAuthor = new Author(foundName, foundAuthorId);
+
+            DB.CloseSqlConnection(rdr, conn);
+
+            return foundAuthor;
+        }
+
+        public static Author Search(string name)
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM author WHERE name = @AuthorName;", conn);
+            cmd.Parameters.Add(new SqlParameter("@AuthorName", name));
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            int foundAuthorId = 0;
+            string foundName = null;
+
+            while(rdr.Read())
+            {
+                foundAuthorId = rdr.GetInt32(0);
+                foundName = rdr.GetString(1);
+            }
+            Author foundAuthor = new Author(foundName, foundAuthorId);
+
+            DB.CloseSqlConnection(rdr, conn);
+
+            return foundAuthor;
+        }
+
+        public void UpdateName(string newName)
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("UPDATE author SET name = @NewName OUTPUT INSERTED.name WHERE id=@AuthorId;", conn);
+
+            SqlParameter newNameParameter = new SqlParameter("@NewName", newName);
+            cmd.Parameters.Add(newNameParameter);
+
+            SqlParameter authorIdParameter = new SqlParameter("@AuthorId", this.GetId());
+            cmd.Parameters.Add(authorIdParameter);
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                this._name = rdr.GetString(0);
+            }
+
+            DB.CloseSqlConnection(rdr, conn);
+        }
+
+        public void Delete()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("DELETE FROM author WHERE id = @AuthorId;", conn);
+            cmd.Parameters.Add(new SqlParameter("@AuthorId", this.GetId()));
+
+            cmd.ExecuteNonQuery();
+
+            if (conn != null)
+            {
+                conn.Close();
+            }
+        }
+
         public static void DeleteAll()
         {
             DB.TableDeleteAll("author");
--- Tests/AuthorTest.cs
+++ Tests/AuthorTest.cs
@@ -40,6 +40,71 @@ namespace Library
             Assert.Equal(testId, result);
         }
 
+        [Fact]
+        public void Test_Find_FindsAuthorInDatabase()
+        {
+            //Arrange
+            Author testAuthor1 = new Author("example author1");
+            testAuthor1.Save();
+
+            Author testAuthor2 = new Author("example author2");
+            testAuthor2.Save();
+
+            //Act
+            Author result = Author.Find(testAuthor2.GetId());
+
+            //Assert
+            Assert.Equal(testAuthor2, result);
+        }
+
+        [Fact]
+        public void Search_AuthorTitle_FoundAuthorInDatabase()
+        {
+            //Arrange
+            Author testAuthor1 = new Author("example author1");
+            testAuthor1.Save();
+
+            Author testAuthor2 = new Author("example author2");
+            testAuthor2.Save();
+
+            //Act
+            Author result = Author.Search(testAuthor2.GetName());
+
+            //Assert
+            Assert.Equal(testAuthor2, result);
+        }
+
+        [Fact]
+        public void UpdateName_OneAuthor_NewName()
+        {
+            Author author1 = new Author("Barcelona");
+            author1.Save();
+            Author author2 = new Author("Honolulu");
+            author2.Save();
+
+            author1.UpdateName("ex author");
+
+            string newName = author1.GetName();
+
+            Assert.Equal(newName, "ex author");
+        }
+
+        [Fact]
+        public void Delete_OneAuthor_AuthorDeleted()
+        {
+            Author testAuthor1 = new Author("Britton");
+            testAuthor1.Save();
+            Author testAuthor2 = new Author("Megan Britney");
+            testAuthor2.Save();
+
+            testAuthor1.Delete();
+
+            List<Author> allAuthors = Author.GetAll();
+            List<Author> expected = new List<Author>{testAuthor2};
+
+            Assert.Equal(expected, allAuthors);
+        }
+
         public void Dispose()
         {
             Author.DeleteAll();
